a5b51e851597779f0cbf31bb209d2225e559058e
doomcaster1917 <113614995+doomcaster1917@users.noreply.github.com>
2023-05-24 10:00:05 +0300

    Delete websocket directory

--- websocket/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    path('', consumers.JoinAndLeave.as_asgi()),
-    path('groups/<int:group_id>/',consumers.GroupConsumer.as_asgi())
-]
--- websocket/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-   path("groups/<int:group_id>/", views.GameGroupAPIView.as_view())
-]
